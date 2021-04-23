985cf502ac1b71cfccc3263e2e337be5c805c79d
Santana-macharia <santanamash@gmail.com>
2021-04-23 18:08:04 +0300

    first commit

---
+++ src/layouts/MainLayout.vue
@@ -0,0 +1,146 @@
+<template>
+  <q-layout view="lHh Lpr lFf">
+    <q-header elevated>
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          icon="menu"
+          aria-label="Menu"
+          @click="leftDrawerOpen = !leftDrawerOpen"
+        />
+
+        <q-toolbar-title>
+          Test
+        </q-toolbar-title>
+        <!-- user section -->
+
+        <div class="q-pa-md" style="max-width: 350px;float:right">
+    <q-expansion-item
+      v-model="expanded"
+      icon="perm_identity"
+      label="User name"
+      caption="Test"
+    >
+      
+    </q-expansion-item>
+  </div>
+  </div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      show-if-above
+      bordered
+      content-class="bg-grey-1"
+    >
+      <q-list>
+        <q-item-label
+          header
+          class="text-grey-8"
+        >
+          PAYMENTS
+        </q-item-label>
+        <EssentialLink
+          v-for="link in essentialLinks"
+          :key="link.title"
+          v-bind="link"
+        />
+        <div class="slimScrollDiv">
+            <div class="sidebar-content" style="height: calc(100vh - 104px);">
+                <ul class="ant-menu ant-menu-inline  ant-menu-light ant-menu-root" role="menu" aria-activedescendant="" tabindex="0">
+                    <li class="ant-menu-item" >
+                        <router-link to="/">
+                            <icon name="home" class="fa-lg"> </icon>
+                            <span class="nav-text"> Transactions </span>
+                        </router-link>
+                    </li>
+                    <li class="ant-menu-item dropdown">
+                        <a href="#" v-on:click.prevent="showDropDown1=!showDropDown1">
+                            <icon name="gears" class="fa-lg"></icon>
+                            <span class="nav-text"> Customers </span>
+                        </a>
+                    </li>
+              
+                        <li class="ant-menu-item">
+                            <a href="/cluster">
+                                <icon name="line-chart" class="fa-lg"></icon>
+                                <span class="nav-text"> Payouts </span>
+                            </a>
+                        </li>
+                   <li class="ant-menu-item" >
+                        <router-link to="/regressor">
+                            <icon name="bar-chart" class="fa-lg"></icon>
+                            <span class="nav-text"> Balances </span>
+                        </router-link>
+                    </li>
+                    <li class="ant-menu-item">
+                        <a href="http://localhost:8088/maps/">
+                            <icon name="map" class="fa-lg"></icon>
+                            <span class="nav-text"> Subscriptions </span>
+                        </a>
+                    </li>
+                
+                    <li class="ant-menu-item dropdown">
+                        <a href="#" v-on:click.prevent="showDropDown=!showDropDown">
+                            <icon name="lock" class="fa-lg"></icon>
+                            <span class="nav-text"> Payment plans </span>
+                        </a>
+                    </li>   
+                    
+                </ul>
+                <q-item-label
+          header
+          class="text-grey-8"
+        >
+          COMMERCE
+        </q-item-label>
+        <ul><li class="ant-menu-item" >
+                        <router-link to="/regressor">
+                            <icon name="bar-chart" class="fa-lg"></icon>
+                            <span class="nav-text"> Referrals </span>
+                        </router-link>
+                    </li>
+                    <li class="ant-menu-item">
+                        <a href="http://localhost:8088/maps/">
+                            <icon name="map" class="fa-lg"></icon>
+                            <span class="nav-text"> Audit logs </span>
+                        </a>
+                    </li>
+                
+                    <li class="ant-menu-item dropdown">
+                        <a href="#" v-on:click.prevent="showDropDown=!showDropDown">
+                            <icon name="lock" class="fa-lg"></icon>
+                            <span class="nav-text"> Settings </span>
+                        </a>
+                    </li>   </ul>
+            </div>
+        </div>
+        
+        
+      </q-list>
+    </q-drawer>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+
+  
+</template>
+
+<script>
+import EssentialLink from 'components/EssentialLink.vue'
+export default {
+  name: 'MainLayout',
+  components: { EssentialLink },
+  data () {
+    return {
+      leftDrawerOpen: false,
+      essentialLinks: linksData
+    }
+  }
+}
+</script>
---
+++ src/pages/Index.vue
@@ -0,0 +1,137 @@
+<template>
+  <q-page >
+     <!-- notifications -->
+  <div class="q-pa-md q-gutter-md">
+   
+
+    <q-btn dense round flat icon="email">
+      <q-badge color="red" floating transparent>
+        4
+      </q-badge>
+    </q-btn>
+
+    
+  </div>
+    
+    <!-- dropdown section -->
+    
+     <div class="q-pa-md">
+    <q-toggle v-model="menu" label="Live" />
+
+    <q-btn-dropdown
+      v-model="menu"
+      class="glossy q-ml-lg"
+      color="primary"
+      label="English"
+    >
+      <q-list>
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section avatar>
+            <q-avatar icon="folder" color="primary" text-color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>French</q-item-label> 
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="info" color="amber" />
+          </q-item-section>
+        </q-item>
+
+        <q-item clickable v-close-popup @click="onItemClick">
+          <q-item-section avatar>
+            <q-avatar icon="assignment" color="secondary" text-color="white" />
+          </q-item-section>
+          <q-item-section>
+            <q-item-label>Spanish</q-item-label>
+          </q-item-section>
+          <q-item-section side>
+            <q-icon name="info" color="amber" />
+          </q-item-section>
+        </q-item>
+      </q-list>
+    </q-btn-dropdown>
+  </div>
+
+  <h6 style="color:#ffd633">Hey User</h6> 
+   <h5>Your dummy dashboard</h5> 
+
+  <!-- circular progress bar -->
+    <div>
+      <h6>Success rate</h6>
+   <q-circular-progress
+      show-value
+      font-size="12px"
+      :value="value"
+      size="50px"
+      :thickness="0.22"
+      color="teal"
+      track-color="grey-3"
+      class="q-ma-md"
+    >
+      {{ value }}%
+    </q-circular-progress></div>
+
+    <!-- chat section -->
+     
+   
+     <div class="q-pa-md row justify-center" style="float:right">
+    <div style="width: 100%; max-width: 400px">
+      <q-card>
+      <q-card-section>
+         <div style="clear: both">
+    <h6 style="float: center">Messages</h6>
+   
+</div>
+<br><br><br><br>
+      <q-chat-message
+        name="James Robinson"
+        stamp="Jan 2, 12:31pm"
+        :text="['I need some software maintenance']"
+        sent
+        text-color="white"
+        bg-color="primary"
+      >
+        <template v-slot:avatar>
+          <img
+            class="q-message-avatar q-message-avatar--sent"
+            src="https://cdn.quasar.dev/img/avatar4.jpg"
+          >
+        </template>
+      </q-chat-message>
+
+      <q-chat-message
+        name="Eseosa Igbinobaro"
+        :text="['I got your email and...']"
+        bg-color="amber"
+      >
+        <template v-slot:avatar>
+          <img
+            class="q-message-avatar q-message-avatar--received"
+            src="https://cdn.quasar.dev/img/avatar2.jpg"
+          >
+        </template>
+
+        <q-spinner-dots size="2rem" />
+      </q-chat-message> </q-card-section>
+    </q-card>
+    </div>
+  </div>
+
+  <!-- bar chart -->
+  
+  
+  </q-page>
+</template>
+
+<script>
+
+export default {
+  data () {
+    return {
+      value: 98
+      
+    }
+  }
+ 
+}
+</script>
